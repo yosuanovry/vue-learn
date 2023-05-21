5a588524573c171a2c1b86c6331a416044e0558d
yosuanovry <yosuanovry@gmail.com>
2023-05-21 13:43:27 +0700

    data structure, props, click e.modifiers, glob css

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <input type="text" ref="username" />
+    <button @click="handleClick">enter</button>
+    <button @click="toggleModal">show modal</button>
+    <div v-if="showModal">
+      <Modal :header="header" :text="text" theme="light" @close="toggleModal"/>
+    </div>
+  </header>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  components: { Modal },
+  data() {
+    return {
+      title: "Learning vue.js",
+      header: "This is a header",
+      text: "This is a text",
+      showModal: false,
+    };
+  },
+  methods: {
+    // handle refs add class to ref
+    handleClick() {
+      this.$refs.username.classList.add("active");
+      this.$refs.username.focus();
+    },
+
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  },
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  width: 100%;
+  text-align: center;
+  background-color: beige;
+  padding: 20px 0px;
+}
+
+header h1 {
+  font-weight: 600;
+  color: cadetblue;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{dark: theme === 'dark', light: theme === 'light'}">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header', 'text', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.modal {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 200px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 15px;
+}
+
+.modal.dark {
+    background: black;
+    color: white;
+}
+
+.modal.light {
+    background: white;
+    color: black;
+}
+
+</style>
